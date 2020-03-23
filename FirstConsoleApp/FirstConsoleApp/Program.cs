1f22682ade11534179aef33960778be5ccb07edb
diwashrestha <diwash712@gmail.com>
2020-03-23 19:08:13 +0545

    Learned methods, class ,Constructors

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to CSharp World!");
+            Console.WriteLine("Press ENTER to continue....");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SchoolLibrary/SchoolLibrary/School.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+
+        private string _twitterAddress;
+        public string twitterAddress
+        {
+            // make sure the twitter address starts with @
+            get { return _twitterAddress; }
+            set
+            {
+                if(value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                }else
+                {
+                    throw new Exception ( "The twitter address must begin with @" );
+                }
+            }
+
+        }
+
+        // Constructor
+        public School()
+        {
+            Name = "Untitled School";
+            Phone = "01-734567";
+        }
+
+        public School(string schoolName, string schoolPhone)
+        {
+            Name = schoolName;
+            Phone = schoolPhone;
+
+        }
+
+        //Methods
+        // Method Overloading
+        public static float averageThreeScores(float a, float b, float c)
+        {
+            var result = (a + b + c ) / 3;
+            return result;
+        }
+
+
+/*        public int averageThreeScores(int a, int b, int c)
+        {
+            var result = (a + b + c) /  3;
+            return result;
+        }*/
+        
+            // function bodied expressions
+        public static int averageThreeScores(int a, int b, int c) => (a + b + c) /3;
+
+
+    }
+}
